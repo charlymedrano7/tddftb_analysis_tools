40f884134e8f81fde5c654612160158d4be51b93
Charly <charlymedrano7@gmail.com>
2020-05-11 11:34:58 +0200

    dynbandpops debugged and documented (please read all the documented lines looking for concept (or not) errors. The bug was related to the form as the script reads the band.out file. There is an empty line after each kpoint info in the file. It was fixed addin '+1' in line 4 block 6 in the .ipynb. I also change the for loop in this block for more clarity.

--- dynbandpops/plot_dynpop_vs_kpts.py
+++ dynbandpops/plot_dynpop_vs_kpts.py
@@ -24,7 +24,7 @@ import subprocess
 root_bands='data_dalma/bands_gs/' #path to band.out and detailed.out files
 root_pops='4.17/pob/'             #path to the molpopuls.dat files
 nplotstates = 50                  #number of bands to be plotted
-timeidxs = [50, 200, 300]         #list of time index to be plotted
+timeidxs = [50, 200, 300]         #list of time index to be plotted (change for something more user friendly)
 circle_size_factor = 1.0e6        #factor for the circles, needed to delta_pops be in the order of 10^3
 
 bandfile = open(root_bands+'band.out', 'r')                                    #open and read the band.out
@@ -36,7 +36,7 @@ idxs = [i for i in range(len(lines)) if 'KPT' in lines[i]]                     #
 
 nstates = idxs[1]-idxs[0]-2                 #the number of states in each kpoint
 nkpt = len(idxs)                            #the number of indxs is the number of kpoints
-                                            #CAREFUL! nkpt is kpt+1 due to the output band.out
+                                            #CAREFUL! nkpt is kpoints+1 due to the output band.out
 kpts = np.arange(0,nkpt)/(nkpt-1) - 0.5     #kpoints
 
 bands = np.zeros((nkpt, nstates),dtype=float)            #bands array 
@@ -55,26 +55,27 @@ fermiidxatgamma = np.argmin(abs(bands[nkpt//2,:]))       #Fermi index at gamma p
                                                          #could be nkpt//2 or (nkpt//2)+1
                                                          #resolved: kpoints is equal to zero at index nkpt//2
 
-#Define the range to be studied:
+#define the range to be studied:
 #centered on the fermi level at gamma point
 #nplotstates/2 upper and nplotstates/2 lower
 states_range = list(range(fermiidxatgamma-nplotstates//2, fermiidxatgamma+nplotstates//2))
 
-np.argmin(abs(kpts))
-
 """
-<plotbandst function> function to plot the band structure automatically
-it needs: ax, kpts, the bands info, the range
+<plotbandst function> function to plot the band structure automatically inside a subplots approach
+needs: ax (index from a list), kpts (array containing the kpoints), bands (array of shape (kpoints,nstates)) 
+info and statesRange (states range to be plotted).
+returns: a plot showing the band structure in the specified range. If the range is not specified the function
+will plot all the states by default.
 """
 def plotbandst(ax, kpts, bands, statesRange=None):       
     try:
-        plotst = len(statesRange)
-    except:
-        statesRange = list(range(bands.shape[1]))
+        plotst = len(statesRange)                    #try to use the statesRange when is provided
+    except:                                          #if not: the statesRange is all the states by
+        statesRange = list(range(bands.shape[1]))    #default (will plot all the states)
     for ist in statesRange:
         ax.plot(kpts, bands[:,ist], c='K', alpha=0.3)
     ax.set_ylim(bands[len(kpts)//2, statesRange[0]], bands[len(kpts)//2, statesRange[-1]])
-    ax.set_xlim(-kpts.max(), kpts.max())     #
+    ax.set_xlim(-kpts.max(), kpts.max())             #setting plot limits
 
 
 fig, ax = plt.subplots(1, 1)
@@ -92,8 +93,8 @@ time = datapops[:,0]
 
 pops = np.zeros((time.size, nkpt, nstates))
 for ik,file in enumerate(molpopuls):
-    datapops = np.genfromtxt(root_pops+file)
-    pops[:,ik,:] = datapops[:,1:]
+    datapops = np.genfromtxt(root_pops+file)       #redefine datapops each step
+    pops[:,ik,:] = datapops[:,1:]                  #constructing pops
 
 nkpt_pops = len(molpopuls)
 if (nkpt_pops == nkpt//2):
@@ -104,36 +105,37 @@ else:
     print('No clear relationship between kpoint grids used for band structure and TD run.')
     sys.exit()
 
+#defining the kpts for the populations calculations
+#(usually different from the kpoints used for band calculations)
 kpts_pops = np.linspace(0, kpts[-2], nkpt_pops)
 
 fig, axx = plt.subplots(1, len(timeidxs), figsize=(6*len(timeidxs),5), sharex=True, sharey=True)
-try:
-    len(axx) > 1.0
-    ax = axx
-except:
+try:                        #just for plt.subplots
+    len(axx) #> 1.0         #if axx is a list (cause len(timeidxs) is grather than 1) can proceed
+    ax = axx                  
+except:                     #if not, ax is a list of one element containing axx
     ax = [axx]
-cols = ['blue', 'red']
-for it,t in enumerate(timeidxs):
-    plotbandst(ax[it], kpts, bands, states_range)                      # BS plot
-    ax[it].set_title('time = {:.1f} fs'.format(time[t])) # set title as time frame
-    for ist in states_range:
-        for ik, kpt in enumerate(kpts_pops):
-            ikb = np.argmin(abs(kpts-kpt))
-            ikb2 = np.argmin(abs(kpts+kpt)) # -(-ik)
-            deltapop = (pops[t,ik,ist]-pops[0,ik,ist])*circle_size_factor   #multiplied by circle factor        
-            coloridx = int(np.sign(deltapop)+1.0)//2
+cols = ['blue', 'red']      #for pops ploting: blue means depopulating, red populating
+for it,t in enumerate(timeidxs):                                  
+    plotbandst(ax[it], kpts, bands, states_range)             #call the plotbandst function for each time index
+    ax[it].set_title('time = {:.1f} fs'.format(time[t]))      #set title as time frame
+    for ist in states_range:                                  #
+        for ik, kpt in enumerate(kpts_pops):                  #BLOCK FOR THE POPS SCATTER PLOTTING
+            
+            ikb = np.argmin(abs(kpts-kpt))    #below Fermi: find the nearest kpoint index in bands corresponding
+                                              #with the kpoint used in pops (needed to scatter plot then)
+            ikb2 = np.argmin(abs(kpts+kpt))   #the same above Fermi.
+            deltapop = (pops[t,ik,ist]-pops[0,ik,ist])*circle_size_factor #calculate corresponding delta pop for
+                                                                          #the time index, kpoint and state
+                                                                          #times by circle factor         
+            coloridx = int(np.sign(deltapop)+1.0)//2          #coloridx for pops plot based on the delta sign
+            #ploting the pops below Fermi
             ax[it].scatter(kpts[ikb], bands[ikb,ist], s=np.sqrt(abs(deltapop)), color=cols[coloridx], edgecolor='k')
+            #Ploting the pops above Fermi
             ax[it].scatter(kpts[ikb2], bands[ikb2,ist], s=np.sqrt(abs(deltapop)), color=cols[coloridx], edgecolor='k') 
     ax[it].set_xticks([kpts.min(), 0, kpts.max()]);
     ax[it].set_xticklabels(['Z', 'G', 'Z'], fontsize=14);
 ax[0].set_ylabel('Energy (eV)',fontsize=16);
 
 plt.savefig('dynbandpops.png', fmt='png', dpi=300, bbox_inches='tight')
-
-
-
-
-
-
-
 
